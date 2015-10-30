e7e4ad60b03eb611cc94629eed068a5125f337dc
Ville Viklund <ville@7am.fi>
2015-10-30 13:49:25 +0200

    Renamed function

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,21 +32,21 @@
             var exercise = $scope.exercises[exercise_name],
                 tm       = $scope.tms[exercise_name];
 
-            exercise.week_1_set_1 = self.roundWeight(tm, WEEK_1_SET_1_FACTOR);
-            exercise.week_1_set_2 = self.roundWeight(tm, WEEK_1_SET_2_FACTOR);
-            exercise.week_1_set_3 = self.roundWeight(tm, WEEK_1_SET_3_FACTOR);
-            exercise.week_2_set_1 = self.roundWeight(tm, WEEK_2_SET_1_FACTOR);
-            exercise.week_2_set_2 = self.roundWeight(tm, WEEK_2_SET_2_FACTOR);
-            exercise.week_2_set_3 = self.roundWeight(tm, WEEK_2_SET_3_FACTOR);
-            exercise.week_3_set_1 = self.roundWeight(tm, WEEK_3_SET_1_FACTOR);
-            exercise.week_3_set_2 = self.roundWeight(tm, WEEK_3_SET_2_FACTOR);
-            exercise.week_3_set_3 = self.roundWeight(tm, WEEK_3_SET_3_FACTOR);
-            exercise.week_4_set_1 = self.roundWeight(tm, WEEK_4_SET_1_FACTOR);
-            exercise.week_4_set_2 = self.roundWeight(tm, WEEK_4_SET_2_FACTOR);
-            exercise.week_4_set_3 = self.roundWeight(tm, WEEK_4_SET_3_FACTOR);
+            exercise.week_1_set_1 = self.calculateSetWeight(tm, WEEK_1_SET_1_FACTOR);
+            exercise.week_1_set_2 = self.calculateSetWeight(tm, WEEK_1_SET_2_FACTOR);
+            exercise.week_1_set_3 = self.calculateSetWeight(tm, WEEK_1_SET_3_FACTOR);
+            exercise.week_2_set_1 = self.calculateSetWeight(tm, WEEK_2_SET_1_FACTOR);
+            exercise.week_2_set_2 = self.calculateSetWeight(tm, WEEK_2_SET_2_FACTOR);
+            exercise.week_2_set_3 = self.calculateSetWeight(tm, WEEK_2_SET_3_FACTOR);
+            exercise.week_3_set_1 = self.calculateSetWeight(tm, WEEK_3_SET_1_FACTOR);
+            exercise.week_3_set_2 = self.calculateSetWeight(tm, WEEK_3_SET_2_FACTOR);
+            exercise.week_3_set_3 = self.calculateSetWeight(tm, WEEK_3_SET_3_FACTOR);
+            exercise.week_4_set_1 = self.calculateSetWeight(tm, WEEK_4_SET_1_FACTOR);
+            exercise.week_4_set_2 = self.calculateSetWeight(tm, WEEK_4_SET_2_FACTOR);
+            exercise.week_4_set_3 = self.calculateSetWeight(tm, WEEK_4_SET_3_FACTOR);
         };
 
-        this.roundWeight = function (tm, factor) {
+        this.calculateSetWeight = function (tm, factor) {
             if (typeof tm === 'undefined') {
                 tm = 0;
             }
